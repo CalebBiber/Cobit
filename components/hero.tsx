3552390b4a452d4818b971fd249560ac706135a3
Caleb <102651139+CalebBiber@users.noreply.github.com>
2024-07-06 21:35:21 +0800

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -21,7 +21,7 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16" id="download">
-            <h1 className="h1 mb-4" data-aos="fade-up">Secure & Easy Wallet</h1>
+            <h1 className="h1 mb-4" data-aos="fade-up">Local Storage & Offline Wallet</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Cobit Wallet is a mobile cryptocurrency wallet that allows users to securely store, manage, and interact with various digital assets.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
